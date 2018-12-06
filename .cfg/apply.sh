e341968d270da0f8cdc8c85be95b361b2a6908c7
Stuckey, William M <william.stuckey.stu@gtri.gatech.edu>
2018-12-06 14:30:20 -0500

    fix nauti glitch on i3

--- .cfg/apply.sh
+++ .cfg/apply.sh
@@ -3,6 +3,10 @@
 # load gnome terminal theme
 dconf load /org/gnome/terminal/ < ~/.gnome_terminal
 
+# stop nautilius from glitching i3
+gsettings set org.gnome.desktop.background show-desktop-icons false
+
+# apply themes
 gsettings set org.gnome.desktop.interface gtk-theme "SolArc-Dark"
 gsettings set org.gnome.desktop.wm.preferences theme "SolArc-Dark"
 gsettings set org.gnome.desktop.interface gtk-color-scheme "SolArc-Dark"
