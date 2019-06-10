76f7175c7f6dfbace7b161595b56e15aad18c301
William Stuckey <guyfleeman@gmail.com>
2019-06-10 14:52:23 -0400

    updates for DE machines

--- .cfg/setup.sh
+++ .cfg/setup.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
+
+set -e
+
 INIT_USER=$(whoami);
 
 # prompt for root
-[ "$INIT_USER" != "root" ] && exec sudo -- "$0" "$@"
+[ "$INIT_USER" != "root" ] && exec sudo -- "$0" $INIT_USER "$@"
+
+INIT_USER=$1
+
+echo "Will install dotfiles for $INIT_USER"
 
 # install packages
 apt-get update -y
@@ -12,12 +19,17 @@ if [ $? -ne 0 ]; then
 	exit
 fi
 
-# set Zsh default sh
-chsh -s $(which zsh) $INIT_USER
+if [ `grep -c "$INIT_USER" /etc/passwd` -eq 0 ]; then
+	echo "Unable to change shell. $INIT_USER not found in local user registery."
+	echo "Is the machine on directory services?"
+else
+	# set Zsh default sh
+	chsh -s $(which zsh) $INIT_USER
+fi
 
 # grab oh-my-zsh
-if [ -d "$HOME/.oh-my-zsh" ]; then
-	git --git-dir=$HOME/.oh-my-zsh --work-tree=$HOME/.oh-my-zsh status
+if [ -d "/home/$INIT_USER/.oh-my-zsh" ]; then
+	git --git-dir=/home/$INIT_USER/.oh-my-zsh/.git status > /dev/null
 	if [ $? -ne 0 ]; then
 		echo "Oh-My-Zsh exists but the repo is in an invalid state."
 		echo "Will not clone."
@@ -25,10 +37,15 @@ if [ -d "$HOME/.oh-my-zsh" ]; then
 else
 	echo "No Oh-My-Zsh folder was found. Will run RR inst."
 	sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-	git clone https://github.com/bhilburn/powerlevel9k.git $HOME/.oh-my-zsh/custom/themes/powerlevel9k
-	chown -R $INIT_USER:$INIT_USER $HOME/.oh-my-zsh
-	chown $INIT_USER:$INIT_USER $HOME/.zshrc
-	chown $INIT_USER:$INIT_USER $HOME/.zsh-history
+	git clone https://github.com/bhilburn/powerlevel9k.git /home/$INIT_USER/.oh-my-zsh/custom/themes/powerlevel9k
+	chown -R $INIT_USER:$INIT_USER /home/$INIT_USER/.oh-my-zsh
+	chown $INIT_USER:$INIT_USER /home/$INIT_USER/.zshrc
+	chown $INIT_USER:$INIT_USER /home/$INIT_USER/.zsh-history
+
+	# restore zshrc
+	if [ -f $HOME/.zshrc.pre-oh-my-zsh ]; then
+		mv $HOME/.zshrc.pre-oh-my-zsh $HOME/.zshrc
+	fi
 
 	echo "Will perform one time install of powerline fonts."
 	git clone https://github.com/powerline/fonts.git --depth=1
@@ -37,6 +54,10 @@ else
 	cd ..
 	rm -rf fonts
 fi
+
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+config submodule init
+config submodule update --recursive
 
 cd external/solarc
 ./autogen.sh --prefix /usr --disable-light --disable-xfwm --disable-cinnamon
